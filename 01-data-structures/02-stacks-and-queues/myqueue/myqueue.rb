34ac643ebbc64f16d10d76e6d367aa2794171ff1
Aizaz Khaja <aizazkj@gmail.com>
2018-01-11 23:01:27 -0500

    myqueue complete

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -5,15 +5,28 @@ class MyQueue
   def initialize
     @queue = Array.new
     @head = @queue[0]
+    @tail = @queue[-1]
   end
 
   def enqueue(element)
-    
+    @queue.insert(-1, element)
+    @head = @queue.first
+    @tail = @queue.last
   end
 
   def dequeue
+    item = @queue
+    @queue.delete_at(0)
+    @head = @queue.first
+    @tail = @queue.last
+    item
   end
 
   def empty?
+    if @queue == []
+      true
+    else
+      false
+    end
   end
 end

a175ca0e39db15aa99d78f36be9449098c3aac43
Aizaz Khaja <aizazkj@gmail.com>
2018-01-11 22:25:02 -0500

    completed mystack

--- 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
+++ 01-data-structures/02-stacks-and-queues/mystack/mystack.rb
@@ -7,11 +7,24 @@ class MyStack
   end
 
   def push(item)
+    # this method should add an element at the end of @stack, an array.
+    @stack.insert(-1, item)
+    self.top = @stack[-1]
   end
 
   def pop
+    # this method should remove an element at the end of @stack and return the item that was popped.
+    item = @stack.last
+    @stack.delete_at(-1)
+    self.top = @stack[-1]
+    item
   end
 
   def empty?
+    if @stack == []
+      true
+    else
+      false
+    end
   end
 end

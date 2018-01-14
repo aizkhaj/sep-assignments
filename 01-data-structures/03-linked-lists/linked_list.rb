892c303133c9d9ad68ae69a1f82fb3cf6b5221e3
Aizaz Khaja <aizazkj@gmail.com>
2018-01-13 20:16:53 -0500

    addtotail method works. progress save.

--- 01-data-structures/03-linked-lists/linked_list.rb
+++ 01-data-structures/03-linked-lists/linked_list.rb
@@ -6,14 +6,30 @@ class LinkedList
 
   # This method creates a new `Node` using `data`, and inserts it at the end of the list.
   def add_to_tail(node)
+    if @tail == nil
+      @tail = node
+      @head == nil ? @head = node : @head.next = @tail
+    else
+      @tail.next = node
+      @tail = node
+    end
   end
 
   # This method removes the last node in the lists and must keep the rest of the list intact.
   def remove_tail
+    @head.next = @tail
+    @tail = nil
+    @tail.next = nil
   end
 
   # This method prints out a representation of the list.
   def print
+    current_node_data = @head
+      
+    while current_node_data != nil
+      puts current_node_data.data
+      current_node_data = current_node_data.next
+    end
   end
 
   # This method removes `node` from the list and must keep the rest of the list intact.

a5671e748c9cbc264752ec0398adbbf4226792e8
Aizaz Khaja <aizazkj@gmail.com>
2018-01-15 01:51:44 -0500

    this assignment is complete. all linked list methods for this class are functional.

--- 01-data-structures/03-linked-lists/linked_list.rb
+++ 01-data-structures/03-linked-lists/linked_list.rb
@@ -17,9 +17,18 @@ class LinkedList
 
   # This method removes the last node in the lists and must keep the rest of the list intact.
   def remove_tail
-    @head.next = @tail
-    @tail = nil
-    @tail.next = nil
+    if @head == @tail
+      @head = @tail = nil
+    elsif @head.next == @tail
+      @head.next = nil
+      @tail = @head
+    else
+      temporary_node = @head
+      while temporary_node.next != @tail
+        temporary_node = temporary_node.next
+      end
+      @tail = temporary_node
+    end
   end
 
   # This method prints out a representation of the list.
@@ -34,13 +43,67 @@ class LinkedList
 
   # This method removes `node` from the list and must keep the rest of the list intact.
   def delete(node)
+    # Here, we search for node location first.
+    track_node = @head
+    if @head == node
+      @head = track_node.next
+    else  
+      while track_node.next != nil && track_node.next != node
+        track_node = track_node.next
+      end
+
+      if track_node.next == @tail
+        @tail = track_node
+      else
+        node_to_replace = track_node.next
+        track_node.next = node_to_replace.next
+      end
+    end
+
+    # The following are some of my previous attempts/incomplete thoughts at achieving this method.
+
+    # if track_node == node
+    #   # if @head == 
+    #     @head = @track_node.next
+    #   # end
+    # end
+
+
+    # # if there's only one node
+    # if @head == @tail && @head || @tail == node
+    #   @head = @tail == nil
+    # elsif @head.next == @tail && @head.next || @tail == node
+    #   @head = @tail
+    #   @head.next == nil
+    # end
+
+    # if @head.next == node
+    #   temporary_node.next = @head.next
+    #   @head.next = nil
+
+    # end
+    # # if there are more than one nodes
+    # while temporary_node.next == nil
+    #   temporary_node = temporary_node.next
+    # end
+    # @head.next = temporary_node
+
   end
 
   # This method adds `node` to the front of the list and must set the list's head to `node`.
   def add_to_front(node)
+    if @head == nil
+      @head = node
+      @tail = node
+    else
+      temporary_node = @head
+      @head = node
+      node.next = temporary_node
+    end
   end
 
   # This method removes and returns the first node in the Linked List and must set Linked List's head to the second node.
   def remove_front
+    @head = @head.next
   end
 end

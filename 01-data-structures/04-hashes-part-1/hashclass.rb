0dad212170159645a2e06620d44aa0a7b382970b
Aizaz Khaja <aizazkj@gmail.com>
2018-01-24 13:05:44 -0500

    progress commit. final error remains on setter method

--- 01-data-structures/04-hashes-part-1/hashclass.rb
+++ 01-data-structures/04-hashes-part-1/hashclass.rb
@@ -5,24 +5,30 @@ class HashClass
   end
 
   def []=(key, value)
-    item = index(key, size)
+    index = index(key, size)
 
-    resize unless @items[item] == nil || @items[item].value == value
-    @items[item] = HashItem.new(key, value)
+    if @items[index] == nil || @items[index].value == value
+      @items[index] = HashItem.new(key, value)
+    else
+      resize
+    end
+    
+    puts "HashClass: #{@items}"
   end
 
 
   def [](key)
-    item = index(key, size)
-    @items[item].value
+    index = index(key, size)
+    @items[index].value
   end
 
   def resize
     old_array = @items
-    @items = Array.new(size * 2)
+    new_size = size * 2
+    @items = Array.new(new_size)
     old_array.each do |item|
-      unless item == nil
-        @items[index(item.key, size)] = item
+      if item != nil
+        @items[index(item.key, new_size)] = item
       end
     end
   end
@@ -32,7 +38,7 @@ class HashClass
   # a starting point.
   def index(key, size)
     sum = 0
-    key.each_byte {|x| sum += x}
+    key.each_codepoint {|x| sum += x}
     # we sum the ascii value of each char on 'key'
     sum % size
     # our hashing function should be % by size of our @items array to deliver a unique ID.

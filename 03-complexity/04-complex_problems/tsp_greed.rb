15ee55b0a161d986736146bd1cb0624fc6827fbd
Aizaz Khaja <aizazkj@gmail.com>
2018-03-17 02:10:14 -0400

    converted pseudocode to implement tsp_greed

---
+++ 03-complexity/04-complex_problems/tsp_greed.rb
@@ -0,0 +1,16 @@
+# implement nearest neighbor (greedy method) for traveling salesman problem in ruby.
+
+def tsp_greed(graph_of_cities, current_city)
+  while current_city.visited == false
+    neighbor_cities = current_city.neighbors
+    next_city = neighbor_cities[0]
+    
+    for current_neighbor in neighbor_cities
+      if current_neighbor.distance < next_city.distance
+        next_city = current_neighbor
+      end
+    end
+    current_city.visited = true
+    current_city = next_city
+  end
+end

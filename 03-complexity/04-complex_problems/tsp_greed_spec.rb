7a17581dcc4f1674c6300acf437d4d28840b65de
Aizaz Khaja <aizazkj@gmail.com>
2018-03-21 23:40:36 -0400

    correctly tested and implemented a greedy traveling salesman solution

--- 03-complexity/04-complex_problems/tsp_greed_spec.rb
+++ 03-complexity/04-complex_problems/tsp_greed_spec.rb
@@ -5,24 +5,28 @@ require_relative 'tsp_greed'
 RSpec.describe Greedy, type: Class do
   let (:greedy) {Greedy.new}
   # City.new(city_name)
-  let (:c1) {City.new('a')}
-  let (:c2) {City.new('b')}
-  let (:c3) {City.new('c')}
-  let (:c4) {City.new('d')}
+  let (:a) {City.new('a')}
+  let (:b) {City.new('b')}
+  let (:c) {City.new('c')}
+  let (:d) {City.new('d')}
   
   before :each do
     # Establish neighbors 
-    c1.neighbors = [Neighbor.new(c2, 25), Neighbor.new(c3, 40)]
-    c2.neighbors = [Neighbor.new(c4, 35), Neighbor.new(c3, 60)]
-    c3.neighbors = [Neighbor.new(c2, 60), Neighbor.new(c1, 40)]
-    c4.neighbors = [Neighbor.new(c3,30), Neighbor.new(c1,50)]
+    a.neighbors = [Neighbor.new(b, 25), Neighbor.new(c, 40)]
+    b.neighbors = [Neighbor.new(d, 35), Neighbor.new(c, 60)]
+    c.neighbors = [Neighbor.new(b, 60), Neighbor.new(a, 40)]
+    d.neighbors = [Neighbor.new(c, 30), Neighbor.new(a, 50)]
   end
 
   describe '#nearest_neighbor_path' do
     it 'returns a correct tour' do
-      result = greedy.nearest_neighbor_path(c1)
+      result = greedy.nearest_neighbor_path(a)
       expect(result).to eq ["a", "b", "d", "c"]
     end
+
+    it 'returns a correct tour again' do
+      result = greedy.nearest_neighbor_path(b)
+      expect(result).to eq ["b", "d", "c", "a"]
+    end
   end
-  # tour should be: ["a", "b", "d", "c"]
 end
